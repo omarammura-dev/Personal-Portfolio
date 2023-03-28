6b087205a5a976b4c47f479f584cf38d2891fc8a
Omar Ammura <omarammoralm10@gmail.com>
2023-03-29 00:38:12 +0300

    edited contact api

--- src/lib/contact.svelte
+++ src/lib/contact.svelte
@@ -1,28 +1,27 @@
 <script>
     import { Input, Label } from 'flowbite-svelte';
-    import { Button } from 'flowbite-svelte';
     import { Textarea } from 'flowbite-svelte'
     import Card from './card.svelte'
-    const handleSubmit = e => {
 		// getting the action url
-		const ACTION_URL = e.target.action
+    let name = "";
+    let surname = "";
+    let email = "";
+    let message = "";
+		const ACTION_URL = 'https://send.pageclip.co/vOf1aijihVi3PCuBhDN1lJQ7VyLSmvuD/myWebsite'
 
-		// get the form fields data and convert it to URLSearchParams
-		const formData = new FormData(e.target)
-		const data = new URLSearchParams()
-		for (let field of formData) {
-			const [key, value] = field
-			data.append(key, value)
-		}
-
-        if (e.target.method.toLowerCase() == 'get') fetch(`${ACTION_URL}?${data}`)
-		else {
-			fetch(ACTION_URL, {
-				method: 'POST',
-				body: data
-			})			
-		}			
+    async function doPost () {
+		const res = await fetch(ACTION_URL, {
+			method: 'POST',
+			body: JSON.stringify({
+				name,
+				surname,
+        email,
+        message
+			})
+		})
 		
+		const json = await res.json()
+		result = JSON.stringify(json)
 	}
 </script>
 
@@ -32,24 +31,24 @@
   </div>
 <div class="px-20">
 
-<form  on:submit|preventDefault={handleSubmit} action="https://send.pageclip.co/vOf1aijihVi3PCuBhDN1lJQ7VyLSmvuD/myWebsite">
+<form  on:submit|preventDefault={doPost} method="post">
     <div class="grid  gap-6 mb-6 md:grid-cols-2">
       <div>
         <Label for="first_name" class="mb-2">First name</Label>
-        <Input type="text" id="first_name" placeholder="John" required  />
+        <Input type="text" id="first_name" bind:value={name} placeholder="John" required  />
       </div>
       <div>
         <Label for="last_name" class="mb-2">Last name</Label>
-        <Input type="text" id="last_name" placeholder="Doe" required />
+        <Input type="text" id="last_name" bind:value={surname} placeholder="Doe" required />
       </div>
     </div>
     <div class="mb-6">
       <Label for="email" class="mb-2">Email address</Label>
-      <Input type="email" id="email" placeholder="john.doe@company.com" required />
+      <Input type="email" id="email" bind:value={email} placeholder="john.doe@company.com" required />
     </div>
     <div class="mb-6">
         <Label for="message" class="mb-2 h-5">Your Message</Label>
-        <Textarea id="message" placeholder="Your message" rows="4" name="message"/>
+        <Textarea id="message" bind:value={message} placeholder="Your message" rows="4" name="message"/>
       </div>
       <button type="submit" class="mx-auto relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800">
         <span class="mx-auto justify-center relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
