4130e80c0b27ab1d88cd05fd2d7fb8be6a759f0a
Omar Ammura <omarammoralm10@gmail.com>
2023-06-23 00:02:14 +0300

    added hire me button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -57,6 +57,13 @@
             </span>
           </button> 
         </a>
+        <a href="https://www.fiverr.com/omarammora">
+          <button href="" class="mx-auto relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800">
+            <span class="mx-auto justify-center relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+              Hire me
+            </span>
+          </button> 
+        </a>
       </div>
     </div>
     <div class="scroll-indicator">
