66232accd2951723dcaa320b43027b180ecca912
Omar Ammura <omarammoralm10@gmail.com>
2023-06-24 14:33:29 +0300

    added testmonial

--- src/lib/testimonial.svelte
+++ src/lib/testimonial.svelte
@@ -32,7 +32,7 @@
    </figure>
    <figure class="flex flex-col justify-center items-center p-8 text-center bg-white rounded-bl-lg border-b border-gray-200 md:border-b-0 md:border-r dark:bg-gray-800 dark:border-gray-700">
        <blockquote class="mx-auto mb-4 max-w-2xl text-gray-500 dark:text-gray-400">
-           <p class="my-4 font-light">Aesthetically, the well-designed components are beautiful and will undoubtedly level up your next application."</p>
+           <p class="my-4 font-light">"Amazing fast and great support :)"</p>
        </blockquote>
        <Rating/>
        <figcaption class="flex justify-center items-center space-x-3">
@@ -43,4 +43,18 @@
            </div>
        </figcaption>
    </figure>
+   <figure class="flex flex-col justify-center items-center p-8 text-center bg-white rounded-bl-lg border-b border-gray-200 md:border-b-0 md:border-r dark:bg-gray-800 dark:border-gray-700">
+    <blockquote class="mx-auto mb-4 max-w-2xl text-gray-500 dark:text-gray-400">
+        <p class="my-4 font-light">"Eng. Omar is a very professional designer for websites and Apps related work. We are relaying on him for all of our requirements on such service. We hope the best for him.
+            "</p>
+    </blockquote>
+    <Rating/>
+    <figcaption class="flex justify-center items-center space-x-3">
+        <img class="w-9 h-9 rounded-full" src="4.png" alt="axrinas">
+        <div class="space-y-0.5 font-medium dark:text-white text-left">
+            <div>Ahmed Said</div>
+            <div class="text-sm font-light text-gray-500 dark:text-gray-400">MYRCLLC CEO</div>
+        </div>
+    </figcaption>
+</figure>
 </Card>
