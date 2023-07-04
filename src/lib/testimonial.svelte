a2f1d280333f04e7c4b465565b7b79bd036cf4fd
Omar Ammura <omarammoralm10@gmail.com>
2023-07-04 19:51:32 +0300

    some design changes

--- src/lib/testimonial.svelte
+++ src/lib/testimonial.svelte
@@ -4,7 +4,7 @@
 </script>
 
 <Card padding="none" size="xl" class="grid md:grid-cols-2">
-   <figure class="flex flex-col justify-center items-center p-8 text-center bg-white rounded-t-lg border-b border-gray-200 md:rounded-t-none md:rounded-tl-lg md:border-r dark:bg-gray-800 dark:border-gray-700">
+   <figure class="flex flex-col justify-center items-center p-8 text-center bg-gray-800 rounded-t-lg border-b border-gray-200 md:rounded-t-none md:rounded-tl-lg md:border-r dark:bg-gray-800 dark:border-gray-700">
        <blockquote class="mx-auto mb-4 max-w-2xl text-gray-500 dark:text-gray-400">
            <p class="my-4 font-light">"the best person with amazing skills, completed the task before the deadline, really happy from the service..."</p>
        </blockquote>
@@ -17,7 +17,7 @@
            </div>
        </figcaption>
    </figure>
-   <figure class="flex flex-col justify-center items-center p-8 text-center bg-white rounded-tr-lg border-b border-gray-200 dark:bg-gray-800 dark:border-gray-700">
+   <figure class="flex flex-col justify-center items-center p-8 text-center bg-gray-800 rounded-tr-lg border-b border-gray-200 dark:bg-gray-800 dark:border-gray-700">
        <blockquote class="mx-auto mb-4 max-w-2xl text-gray-500 dark:text-gray-400">
            <p class="my-4 font-light">"Professional and smart person. he performed the work perseveringly and perfectly, in a problem that seemed unsolvable and went beyond expectations for sure in the future I will work with him again more than recommended if you want a competent and serious person you have chosen the right person for a job"<br/>(Translated from italian)</p>
        </blockquote>
@@ -30,7 +30,7 @@
            </div>
        </figcaption>
    </figure>
-   <figure class="flex flex-col justify-center items-center p-8 text-center bg-white rounded-bl-lg border-b border-gray-200 md:border-b-0 md:border-r dark:bg-gray-800 dark:border-gray-700">
+   <figure class="flex flex-col justify-center items-center p-8 text-center bg-gray-800 rounded-bl-lg border-b border-gray-200 md:border-b-0 md:border-r dark:bg-gray-800 dark:border-gray-700">
        <blockquote class="mx-auto mb-4 max-w-2xl text-gray-500 dark:text-gray-400">
            <p class="my-4 font-light">"Amazing fast and great support :)"</p>
        </blockquote>
@@ -43,7 +43,7 @@
            </div>
        </figcaption>
    </figure>
-   <figure class="flex flex-col justify-center items-center p-8 text-center bg-white rounded-bl-lg border-b border-gray-200 md:border-b-0 md:border-r dark:bg-gray-800 dark:border-gray-700">
+   <figure class="flex flex-col justify-center items-center p-8 text-center bg-gray-800 rounded-bl-lg border-b border-gray-200 md:border-b-0 md:border-r dark:bg-gray-800 dark:border-gray-700">
     <blockquote class="mx-auto mb-4 max-w-2xl text-gray-500 dark:text-gray-400">
         <p class="my-4 font-light">"Eng. Omar is a very professional designer for websites and Apps related work. We are relaying on him for all of our requirements on such service. We hope the best for him.
             "</p>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -20,7 +20,7 @@
 </script>
 
 <Head/>
-<div class="container mx-auto px-4">
+<div class="container px-4 float-left flex-container ml-39 my-4 mt-10">
   <section class="full-screen d-flex justify-content-center align-items-center">
     <ul class="circles">
       <li></li>
@@ -43,7 +43,7 @@
         <p class="text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400 text-center py-6">Creative Web and Mobile App Developer with a talent for problem-solving</p>
       </div>
       <div class="buttons">
-        <a href="/resume.pdf">
+        <a href="/MyResume.pdf">
           <button class="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800">
             <span class="justify-center relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
               Download CV
